fcfab1608d4dcce529887f983811f20406c4197e
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-01-11 19:21:58 -0800

    rename to payroll

--- java-kubectl/payroll/src/main/java/payroll/Application.java
+++ java-kubectl/payroll/src/main/java/payroll/Application.java
@@ -1,4 +1,4 @@
-package hello;
+package payroll;
 
 import java.util.Arrays;
 
--- java-kubectl/payroll/src/main/java/payroll/PayrollController.java
+++ java-kubectl/payroll/src/main/java/payroll/PayrollController.java
@@ -1,11 +1,11 @@
-package hello;
+package payroll;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
